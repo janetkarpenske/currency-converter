6a89d34a3c2bcb9a57b4a4250a94309e16568446
Janet Karpenske <janetkarpenske@gmail.com>
2020-09-11 09:47:59 -0700

    Fix url

--- src/js/conv-service.js
+++ src/js/conv-service.js
@@ -2,7 +2,7 @@ export class ConverterService {
   static getExchange(dollars, currType) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = "https://v6.exchangerate-api.com/v6/17cbb051b9f2e2655d10f97b/latest/USD";
+      const url = `https://v6.exchangerate-api.com/${process.env.API_KEY}/latest/USD`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
